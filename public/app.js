2efeb0e46b622842b7543ba1c10845aa447b2423
UnknownIP <experimentaluse127@gmail.com>
2024-10-19 15:46:00 +0500

    Changes in app.js

--- public/app.js
+++ public/app.js
@@ -12,7 +12,7 @@ const options = ["PC", "LED", "Mouse", "Cables", "Laptop", "Keyboard"];
 // ---------- Customers Data Fucntion
 async function countCustomersFromDB() {
   try {
-    let res = await axios.get("http://localhost:3000/data/customers/count");
+    let res = await axios.get("https://store-webapp-production-50b7.up.railway.app/data/customers/count");
     return res.data;
   } catch (error) {
     console.log("ERROR: ", error);
@@ -110,7 +110,7 @@ function calcPrice(event) {
 // ---------- Match Item in List Function
 async function getItemsFromDB() {
   try {
-    let res = await axios.get("http://localhost:3000/data/items");
+    let res = await axios.get("https://store-webapp-production-50b7.up.railway.app/data/items");
     return res.data;
   } catch (error) {
     console.log("ERROR: ", error);
