2df1a3fcb09cb6b9d30f95dc6fb99e0d864c1dce
UnknownIP <experimentaluse127@gmail.com>
2024-10-19 15:28:27 +0500

    Changes in index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // const connection = mysql.createConnection({
 //   host: "localhost",
